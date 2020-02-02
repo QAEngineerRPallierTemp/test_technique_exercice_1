759d5888d72638f341bfb28264d8fe1bc72ced7c
QAEngineerRPallierTemp <qaengineerrpalliertemp@protonmail.com>
2020-02-02 16:28:25 +0100

    Initialisation du projet via mix new + test de vérification du new Suite à l'apprentissage de Elixir début de l'écriture du script de l'exercice 1

---
+++ lib/test_technique_exercice1.ex
@@ -0,0 +1,18 @@
+defmodule TestTechniqueExercice1 do
+  @moduledoc """
+  Documentation for `TestTechniqueExercice1`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TestTechniqueExercice1.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/test_technique_exercice1_test.exs
@@ -0,0 +1,8 @@
+defmodule TestTechniqueExercice1Test do
+  use ExUnit.Case
+  doctest TestTechniqueExercice1
+
+  test "greets the world" do
+    assert TestTechniqueExercice1.hello() == :world
+  end
+end
